b9e0a3399371086bd7b6138a6fb38787df34086e
fablnt <fabio.lentini@mail.polimi.it>
2025-05-22 15:40:24 +0000

    checkpoint and test scripts

---
+++ test0.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime
+import sys
+import gc
+
+
+a = 0
+while True:
+    a = a+1
+    print(a)
+    time.sleep(1)
+        
---
+++ test1.py
@@ -0,0 +1,14 @@
+import time
+from datetime import datetime
+import sys
+import dmtcp
+import gc
+
+
+
+a=0
+while True:
+    a += 1
+    print(a)
+    time.sleep(1)
+
